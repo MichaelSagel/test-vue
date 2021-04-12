705b1c8f701b019f9ca8ae1dbe6731a89d17dd19
bojar <michael.sagel89@gmail.com>
2021-04-12 14:27:03 +0200

    Misha uchi git

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,27 @@
 
     </div>
 
-      <div class="text_container">Plase use your credentials to log in. If you have forgotten your password, please click the "Forgot password" button.</div>
+      <div class="text_container">
+        Plase use your credentials to log in. If you have forgotten your password, please click the "Forgot password" button.
+      </div>
 
-      <div class="yourEmailAddress_container"><input v-model="message" placeholder="Your e-mail address"></div>
+      <div class="yourEmailAddress_container">
+        <input class="yourEmailAddress_container_body" placeholder="Your e-mail address">
+      </div>
                 
-      <div class="yourPassword_container"><input v-model="message" placeholder="Your password"></div>
+      <div class="yourPassword_container">
+        <input class="yourPassword_container_body" placeholder="Your password">
+      </div>
 
-      <div class="forgotPassword_container">Forgot password</div>
+      <div class="forgotPassword_container">
+        Forgot password
+        </div>
 
-      <div class="letsGetStarted_container"><button>→Let´s get started!</button></div>
+      <div class="letsGetStarted_container">
+        <button class="letsGetStarted_container_bady">
+          →Let´s get started!
+        </button>
+      </div>
 
    </div>
 
@@ -61,9 +73,18 @@
   margin:25px 25px;
   font-size: 20pt;
 }
+.yourEmailAddress_container_body{
+  font-size: 20pt;
+  width: 100%;
+}
 
 .yourPassword_container{
   margin:25px 25px;
+  font-size: 20pt;
+}
+
+.yourPassword_container_body{
+  width: 100%;
   font-size: 20pt;
 }
 
@@ -76,6 +97,14 @@
 
 .letsGetStarted_container{
   margin:25px 25px;
+  
+}
+
+.letsGetStarted_container_bady{
+  width: 100%;
+  font-size: 20pt;
+  display: flex;
+  justify-content: space-between;
 }
 
 </style>
