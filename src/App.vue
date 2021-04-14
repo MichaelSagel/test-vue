40a7b757fca57f6ddc51735ba3b29e50c5309da8
bojar <michael.sagel89@gmail.com>
2021-04-14 12:29:09 +0200

    Misha uchi git

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,68 @@
 <template>
-  <div class="container">
 
-      <div class="login">
-          Login
-        <button class="button_bady">
-          Create New Accaunt
-        </button>
+  <div class="layout">
+    
+    <header class="epx-authentication__header">
+      <img src="https://virtrex-vtf-stage-files.fra1.digitaloceanspaces.com/files/SgAHSsaBTeFU322LvQGTCQ7xwO7dNhMyfvDuTyfh.png?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&amp;X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=ZXTMNN6WRZVJBDDTCWDO%2F20210414%2Ffra1%2Fs3%2Faws4_request&amp;X-Amz-Date=20210414T084519Z&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Expires=7200&amp;X-Amz-Signature=46fb4d27c956d4ec4c618fa50e8e86816ad628cc7ef85cb309584e723202e13c" alt="Virtrex" class="img-gira">
+    </header>  
+
+    
+    
+
+
+
+
+
+
+
+
+
+    <div class="container">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="login">
+            Login
+          <a class="button_bady" href="">
+            Create New Accaunt
+          </a>
+        </div>
+
+      <div class="container-body">
+        Plase use your credentials to log in. If you have forgotten your password,
+        please click the "Forgot password" button.
       </div>
 
-    <div class="container-body">
-      Plase use your credentials to log in. If you have forgotten your password,
-      please click the "Forgot password" button.
-    </div>
+      <div class="container-body">
+        <input class="input_container_body" placeholder="Your e-mail address" />
+      </div>
 
-    <div class="container-body">
-      <input class="input_container_body" placeholder="Your e-mail address" />
-    </div>
+      <div class="container-body">
+        <input class="input_container_body" placeholder="Your password" />
+      </div>
 
-    <div class="container-body">
-      <input class="input_container_body" placeholder="Your password" />
-    </div>
+      <div class="forgotPassword_container">
+        <a class="button_bady" href="">
+          Forgot password
+        </a>
+      </div>
 
-    <div class="forgotPassword_container">
-      <button class="button_bady">
-        Forgot password
-      </button>
-    </div>
-
-    <div class="container-body">
-      <button class="letsGetStarted_container_bady">
-        →Let´s get started!
-      </button>
+      <div class="container-body">
+        <button class="letsGetStarted_container_bady">
+          →Let´s get started!
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,15 +73,48 @@
 
 <style>
 
+.layout {
+  position: relative;
+  margin: 16px;
+}
+
+.img-gira {
+  height: 44px;
+}
+
 .container {
   display: block;
   margin: auto;
-  position: relative;
   font-size: 20pt;
-  border: 4px ridge rgb(26, 207, 207);
+  border: 4px ridge gray;
   border-radius: 8px;
   max-width: 800px;
 }
+
+
+
+
+
+
+.vtx-typo-header-2 {
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 32px;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 .login {
   display: flex;
@@ -76,7 +141,13 @@
   margin: 25px 25px;
   background: transparent;
   border: inherit;
+  border-radius: 8px;
 }
+
+.button_bady:hover {
+  background-color: green;
+}
+
 
 .letsGetStarted_container_bady {
   display: flex;
@@ -87,4 +158,9 @@
   border-style: solid;
   border-color: rgb(56, 151, 56);
 }
+
+.letsGetStarted_container_bady:hover {
+  background-color: green;
+}
+
 </style>
