2e6ecef4fff5a65e0e8252157c9524a874dbab4d
bojar <michael.sagel89@gmail.com>
2021-05-01 14:12:27 +0200

    training

--- src/App.vue
+++ src/App.vue
@@ -15,54 +15,54 @@
           </path>
         </svg>
       </div>
+      
+        <div class="container">
 
-      <div class="container">
+          <div class="container-bady">
 
-        <div class="container-bady">
+            <div class="login">
+              Login
+              <a class="button-bady createNewAccaunt" href="">
+                Create New Accaunt
+              </a>
+            </div>
 
-          <div class="login">
-            Login
-            <a class="button-bady createNewAccaunt" href="">
-              Create New Accaunt
-            </a>
-          </div>
+            <div class="container-body-text">
+              Plase use your credentials to log in. If you have forgotten your password,
+              please click the "Forgot password" button.
+            </div>
 
-          <div class="container-body-text">
-            Plase use your credentials to log in. If you have forgotten your password,
-            please click the "Forgot password" button.
-          </div>
+            <div class="container-body">
+              <label>
+                <div class="input-container-body">
+                  Your e-mail address
+                </div>
+                <input class="input-container-footer" placeholder="Your e-mail address"/>
+              </label>  
+            </div>
 
-          <div class="container-body">
-            <label>
-              <div class="input-container-body">
-                Your e-mail address
-              </div>
-              <input class="input-container-footer" placeholder="Your e-mail address"/>
-            </label>  
-          </div>
+            <div class="container-body">
+              <label>
+                <div class="input-container-body">
+                  Your password
+                </div>
+                <input class="input-container-footer" placeholder="Your password"/>
+              </label>  
+            </div>
 
-          <div class="container-body">
-            <label>
-              <div class="input-container-body">
-                Your password
-              </div>
-              <input class="input-container-footer" placeholder="Your password"/>
-            </label>  
-          </div>
+            <div class="forgotPassword-container">
+              <a class="button-bady" href="">
+                Forgot password
+              </a>
+            </div>
 
-          <div class="forgotPassword-container">
-            <a class="button-bady" href="">
-              Forgot password
-            </a>
-          </div>
-
-          <div class="container-body-button">
-            <button class="letsGetStarted-container-bady">
-              →Let´s get started!
-            </button>
+            <div class="container-body-button">
+              <button class="letsGetStarted-container-bady">
+                →Let´s get started!
+              </button>
+            </div>
           </div>
         </div>
-      </div>
 
       <div class=footer>
         <div class="virtex">
